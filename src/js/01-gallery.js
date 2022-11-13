b5a7f228e564f869f48c43f84df5d0d8a012290d
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-13 15:11:19 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,9 +9,9 @@ console.log(galleryItems);
 const listEl = document.querySelector('.gallery');
 
 const listTemplate = (preview, original, description) => `
-    <a class="gallery__item" href="${original}">
+    <div><a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
+    </a></div>
 `;
 
 // listTemplate(galleryItems[0].preview, galleryItems[0].original, galleryItems[0].description);
