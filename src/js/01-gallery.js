7a667389a20cd11f7dad13eae979de98a044de8a
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-13 14:57:49 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 
+const listEl = document.querySelector('.gallery');
 
+const listTemplate = (preview, original, description) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`;
+
+// listTemplate(galleryItems[0].preview, galleryItems[0].original, galleryItems[0].description);
+
+const render = () => {
+    const arrayFromListTemplate = galleryItems.map(item => listTemplate(item.preview, item.original, item.description));
+
+    listEl.innerHTML = arrayFromListTemplate.join('');
+};
+render();
+
+const galleryEl = new SimpleLightbox('ul.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,   
+});
