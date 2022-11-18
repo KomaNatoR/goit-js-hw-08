145ffd49994d599442a1c0c7ddd47c968ff13761
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-18 12:07:23 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,33 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input[type="email"]'),
-  message: document.querySelector('textarea[name="message"]'),
-};
+  formEl: document.querySelector('.feedback-form'),
+  mailEl: document.querySelector('[name="email"]'),
+  textEl: document.querySelector('[name="message"]'),
+}; 
+// console.log(refs.textEl);
 
-refs.form.addEventListener('submit', onFormSumbit);
-refs.form.addEventListener('input', throttle(onInput, 500));
+const KYE_FROM_DATAOBJECT = "feedback-form-state";
+let formData = {};
 
-const STORAGE_EMAIL_KEY = 'email';
-const STORAGE_MESSAGE_KEY = 'message';
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onInputSaveData, 500));
 
-getLocalStorageItems();
+if (localStorage.getItem(KYE_FROM_DATAOBJECT)) {
+  formData = JSON.parse(localStorage.getItem(KYE_FROM_DATAOBJECT));
+  refs.mailEl.value = formData.email;
+  refs.textEl.value = formData.message;
+}
 
-function onFormSumbit(e) {
+function onInputSaveData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(KYE_FROM_DATAOBJECT, JSON.stringify(formData));
+
+    console.dir(formData);
+}
+function onFormSubmit(e) {
   e.preventDefault();
-  consoleFormData(e.currentTarget);
+  console.log(formData);
   e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_EMAIL_KEY);
-  localStorage.removeItem(STORAGE_MESSAGE_KEY);
-}
-
-function onInput(e) {
-  if (e.target.name === 'email')
-    localStorage.setItem(STORAGE_EMAIL_KEY, e.target.value);
-  if (e.target.name === 'message')
-    localStorage.setItem(STORAGE_MESSAGE_KEY, e.target.value);
-}
-
-function getLocalStorageItems(props) {
-  if (localStorage.getItem(STORAGE_EMAIL_KEY)) {
-    refs.email.value = localStorage.getItem(STORAGE_EMAIL_KEY);
-  }
-  if (localStorage.getItem(STORAGE_MESSAGE_KEY)) {
-    refs.message.value = localStorage.getItem(STORAGE_MESSAGE_KEY);
-  }
-}
-
-function consoleFormData(form) {
-  const feedbackData = {};
-  new FormData(form).forEach((value, key) => (feedbackData[key] = value));
-  console.log('feedbackData', feedbackData);
+  localStorage.removeItem(KYE_FROM_DATAOBJECT);
 }
